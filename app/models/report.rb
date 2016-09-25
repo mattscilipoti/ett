8f227aacac3fae253f4a26326dd9901a7117c6d3
Matt Scilipoti <matt@scilipoti.name>
2016-09-25 00:37:09 -0400

    report: require started_at
    - added accept_values_for

--- app/models/report.rb
+++ app/models/report.rb
@@ -1,2 +1,3 @@
 class Report < ApplicationRecord
+  validates_presence_of :started_at
 end
--- spec/models/report_spec.rb
+++ spec/models/report_spec.rb
@@ -1,5 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe Report, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'validation' do
+    subject(:user) { Report.new }
+
+    it { should accept_values_for(:started_at, '2016-01-13', '2016/02/14') }
+    it { should_not accept_values_for(:started_at, nil, " ", '01/13/2016', 'john', 'john@example') }
+  end
 end
