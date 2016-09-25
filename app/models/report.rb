9fabc16d5ce870787c94a951414da80bc94860b3
Matt Scilipoti <matt@scilipoti.name>
2016-09-25 01:12:44 -0400

    report: starts_at 9am

--- app/models/report.rb
+++ app/models/report.rb
@@ -1,3 +1,21 @@
 class Report < ApplicationRecord
+  after_initialize :set_defaults
   validates_presence_of :started_at
+
+  def first_hour
+    9
+  end
+
+  def start_time
+    self.started_at.to_time
+  end
+
+
+  private
+
+  def set_defaults
+    if self.new_record?
+      self.started_at ||= DateTime.current.change({ hour: first_hour })
+    end
+  end
 end
--- spec/models/report_spec.rb
+++ spec/models/report_spec.rb
@@ -7,4 +7,10 @@ RSpec.describe Report, type: :model do
     it { should accept_values_for(:started_at, '2016-01-13', '2016/02/14') }
     it { should_not accept_values_for(:started_at, nil, " ", '01/13/2016', 'john', 'john@example') }
   end
+
+  describe 'defaults' do
+    it 'started_at to today at 9am' do
+      expect(Report.new.started_at).to be_within(1).of(DateTime.current.change({ hour: 9 }))
+    end
+  end
 end
