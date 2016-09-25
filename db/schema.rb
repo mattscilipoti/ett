286343f7be59795fc9cf5c0fc5757b1895279182
Matt Scilipoti <matt@scilipoti.name>
2016-09-25 16:18:07 -0400

    $ rails g model Task name:string
    - name is required

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160925041031) do
+ActiveRecord::Schema.define(version: 20160925201636) do
 
   create_table "reports", force: :cascade do |t|
     t.datetime "started_at", null: false
     t.string   "memo"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "tasks", force: :cascade do |t|
+    t.string   "name",       null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
