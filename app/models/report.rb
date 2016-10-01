c0b63038b0799b975c79739cd44daafab9c19cdc
Matt Scilipoti <matt@scilipoti.name>
2016-09-30 23:06:28 -0400

    report#show
    - possible_occurences used to show all time periods

--- app/models/report.rb
+++ app/models/report.rb
@@ -1,32 +1,40 @@
 class Report < ApplicationRecord
-  has_many :occurrences
-  has_many :tasks, through: :occurrences
+  has_many :occurrences, dependent: :destroy
+  has_many :tasks, -> { distinct }, through: :occurrences
   validates_presence_of :started_at
+
+  def duration
+    9.hours
+  end
 
   def occurrence_duration
     15.minutes
   end
 
-  def possible_occurrences(task)
+  def possible_occurrences(task, report_duration = self.duration)
+    # logger.debug("po.task: #{task}")
     task = case task
     when String
-      Task.new(name: task)
+      Task.find_or_initialize_by(name: task)
     when Task
       task
     else
       fail ArgumentError.new("Unsupported task type (#{task}).")
     end
 
-    @possible_occurrences ||= begin
+    # @possible_occurrences ||= begin
       stepper = started_at
-      end_time = started_at + 9.hours
+      end_time = started_at + report_duration
       possibilites = []
       while stepper < end_time
-        possibilites << occurrences.build(task: task, started_at: stepper, duration: occurrence_duration)
+        logger.debug("stepper: #{stepper}")
+        occurrence = occurrences.where(task: task, started_at: stepper, duration: occurrence_duration).first_or_initialize
+        logger.debug("occurence: #{occurrence.new_record?}")
+        possibilites << occurrence
         stepper += occurrence_duration
       end
       possibilites
-    end
+    # end
   end
 
   def self.with_start_time(start_hour=9)
